85aeccf1e8aeed307fd84e4c9752184c291233ae
Frank Law <frank.law@workday.com>
2011-09-23 17:01:19 -0400

    Empty project

---
+++ Moxie/Moxie/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Moxie
+//
+//  Created by Frank Law on 9/23/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
