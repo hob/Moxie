6cce57c695a0c45af2ec26d025570ea2d6605baf
Frank Law <frank.law@workday.com>
2011-09-23 17:09:32 -0400

    Initial view shows

---
+++ Moxie/Moxie/QuestionViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuestionViewController.h
+//  Moxie
+//
+//  Created by Frank Law on 9/23/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionViewController : UIViewController
+
+@end
---
+++ Moxie/Moxie/QuestionViewController.m
@@ -0,0 +1,66 @@
+//
+//  QuestionViewController.m
+//  Moxie
+//
+//  Created by Frank Law on 9/23/11.
+//  Copyright (c) 2011 . All rights reserved.
+//
+
+#import "QuestionViewController.h"
+
+@implementation QuestionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+    [super loadView];
+    
+    UILabel *test = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 125, 35)];
+    test.text = @"Frank";
+    [self.view addSubview:test];
+}
+
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+*/
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
